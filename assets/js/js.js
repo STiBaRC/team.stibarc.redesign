14be8a6ebe1cf831614185f9494f25be1ffec155
Sophie <me@flolon.cc>
2025-07-25 17:34:44 -0400

    small updates

--- assets/js/js.js
+++ assets/js/js.js
@@ -12,7 +12,7 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("na").style.backgroundColor = "#2c63a5";
+    document.getElementById("na").style.backgroundColor = "#49B9CA";
     document.getElementById("na").style.boxShadow = "0px 5px 20px rgba(40, 40, 40, .3)";
   } else {
     document.getElementById("na").style.backgroundColor = "transparent";
